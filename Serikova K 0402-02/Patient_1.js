8e3073e9e2885dfdc6cbc82dcdb3b57528cbd5a5
SerikovaKseniya <103933473+SerikovaKseniya@users.noreply.github.com>
2022-05-24 10:30:03 +0300

    Add files via upload

---
+++ Serikova K 0402-02/Patient_1.js
@@ -0,0 +1,22 @@
+class Ptient{
+    constructor(f, i, o, d, s, sh) {
+        this.F = f;
+        this.I=i;
+        this.O=o;
+        this.D=new Date(d);
+        this.S=s;
+        this.SH=sh;
+}
+setById() {
+document.getElementById("F").textContent=this.F;
+document.getElementById("I").textContent=this.I;
+document.getElementById("O").textContent=this.O;
+document.getElementById("Y").textContent=this.D.getFullYear();
+document.getElementById("S").textContent=this.S;
+document.getElementById("SH").textContent=this. SH;
+}
+}
+let Ivanov=new Patient('Иванов', 'Иван', 'Иванович', new Date(1980, 6,10), "м", "Назначено");
+let Petrov=new Patient("Петров", "Антон", "Николаевич", new Date(1970, 7,3, "м", "Назначено")
+let Sidorov=new Patient("Сидоров", "-", "-", new Date(1960,8,9, "м", "Назначено)"
+Ivanov.setById();
---
+++ Serikova K 0402-02/java-1.js
@@ -0,0 +1,11 @@
+"Привет Java"
+var myThing;
+myThing=5;
+myThing="это строка"
+var numbers = 9
+var stringNine = "9"
+numberNine + numberNine
+stringNine + stringNine
+var greeting = "Привет, "
+var myName = "Ник"
+greeting + myName
